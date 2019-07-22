9257f71a50443fe98f6b0cc6f11ecf713dd6e3f6
Mahmoud Bwedany <mahmoudbwedany@gmail.com>
2019-07-22 01:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 class BoardMember {
-  constructor
+  constructor (name, homeState, training){
+    this.name = name;
+    this.homeState =homeState;
+    this.training = training;
+  }
+  
+  BoardMember.prototype.veto = Function () {
+    return "No, I must disagree"
+  }
+  
+  BoardMember.prototype.approve = Function(){
+    return "You can do that!"
+  }
+  
+  B
   
   
   
