93da693089c93347c67f2c4ee51566c3a757a2a0
Mahmoud Bwedany <mahmoudbwedany@gmail.com>
2019-07-22 01:38:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ class BoardMember {
     this.training = training;
   }
   
-  BoardMember.prototype.veto = function () {
+  veto () {
     return "No, I must disagree"
   }
   
-  BoardMember.prototype.approve = function(){
+  approve(){
     return "You can do that!"
   }
   
-  BoardMember.prototype.doCharity = function () {
+  doCharity() {
     return "I like to help people."
   }
-  BoardMember.prototype.releasePressStatement = function () {
+  releasePressStatement() {
     return "You will see great thingsfrom Scuber"
   }
-  BoardMember.prototype.sayHi = function () {
+  sayHi () {
     return ("Hi, my name is " + this.name + "." + " I am from " + this.homeState + ", and I was trained in " + this.training + ".");
   }
 }
@@ -27,7 +27,7 @@ class BoardMember {
 
 class Ceo extends BoardMember {
   
-  Ceo.prototype.addEmployee = function () {
+  addEmployee() {
     return "Welcome aboard!"
   }
   
